699964290ab06084f0c3e7356b91dd9f2ee3cfc8
GyDeen <zde421217757@outlook.com>
2026-09-15 15:46:18 +1000

    Remove completeness test

--- verification/java_arrays/generated/drivers/mutant_0001.c
+++
@@ -1,4 +0,0 @@
-/* Generated verification driver: fixed contracts + one mutant. */
-#include "../../contracts/helpers.acsl.h"
-#include "../../contracts/jintarray.acsl.h"
-#include "../mutants/0001.c"
